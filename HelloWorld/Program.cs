6188e581eb24c21b094e176ba64f8f78f0e277e7
Pedro Novaes <pe.fer@hotmail.com>
2021-11-02 23:51:54 -0300

    Meu primeiro commit

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Escreva seu nome: ");
+            var nome = Console.ReadLine();
+            Console.WriteLine("seu nome é: ");
+            Console.WriteLine(nome);
+
+            Console.ReadLine();
+
+
+
+
+
+           
+
+
+
+
+        }
+    }
+}
